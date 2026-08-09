6e9e6a4af67bc87f706e7ead14f60dff574761a8
nafania2026 <nafania2026@gmail.com>
2026-08-10 01:38:04 +0300

    Add files via upload

---
+++ lesson_1_task_3.py
@@ -0,0 +1,12 @@
+first_name = input("Вас зовут:first_name").split()
+
+last_name = input("Вас зовут:last_name").split()
+
+first_name,last_name = input("Вас зовут:last_name first_name").split()
+
+
+
+
+
+
+
---
+++ lesson_1_task_5.py
@@ -0,0 +1,14 @@
+numbers = [88005553535]
+for num in numbers:
+    print (num * 1)
+    print (num * 1)
+    print (num * 1)
+    print (num * 1)        
+    print (num * 1)
+    print (num * 1)
+    print (num * 1)
+    print (num * 1)
+    print (num * 1)
+    print (num * 1)
+    print (num * 1)
+       
