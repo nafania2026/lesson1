2f3765b1e3b69caa4926229b4332f3706bd164a6
nafania2026 <nafania2026@gmail.com>
2026-08-30 00:05:26 +0300

    добавлены файлы lesson_2

---
+++ lesson_2_task_4.py
@@ -0,0 +1,14 @@
+def fizz_buzz (n):
+   for n in range(1,n+1):
+    if n % 3 == 0 and n % 5 == 0: 
+        print("FizzBuzz")
+    elif n % 5 == 0: 
+        print("Buzz")
+    elif n % 3 == 0 and n % 5 == 0: 
+        print("FizzBuzz")
+    elif n % 3 == 0: 
+        print("Fizz")   
+    else:
+       print(n)
+
+fizz_buzz(30)
---
+++ lesson_2_task_7.py
@@ -0,0 +1,4 @@
+lst=[18,14,10,6,2]
+l=len(lst)
+for y in range (0,l):
+   print(lst[y])
