6e81a5a3d2e987d6c81cb7d377a8d1bb118cbb84
nafania2026 <nafania2026@gmail.com>
2026-08-30 18:00:13 +0300

    Add files via upload

---
+++ 02_lesson/lesson_2_task_2.py
@@ -0,0 +1,5 @@
+import calendar
+year = 2024
+is_year_leap= calendar.isleap(year)
+result = calendar.isleap(year)
+print(f"год{year}:{result}")
---
+++ 02_lesson/lesson_2_task_5.py
@@ -0,0 +1,14 @@
+def month_to_season(month_number):
+    if month_number in(12,1,2):
+        return "Зима"
+    elif month_number in(3,4,5):
+        return "Весна"
+    elif  month_number in (6,7,8):
+        return "Лето"
+    elif  month_number in(9,10,11):
+        return "Осень"
+    else:
+         return "Некорректный номер месяца"
+        
+print(month_to_season(9))
+    
